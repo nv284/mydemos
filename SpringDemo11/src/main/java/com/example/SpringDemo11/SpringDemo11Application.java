5cce4962337a40877bc5e6e87444a9c1cc9c694a
nv284 <creativeuser284@gmail.com>
2023-08-29 12:40:57 +0530

    second commit

---
+++ SpringDemo11/src/main/java/com/example/SpringDemo11/SpringDemo11Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringDemo11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo11Application.class, args);
+	}
+
+}
